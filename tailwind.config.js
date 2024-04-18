0a90eb067dac8b317b3ac3f9989efa9ece925fe1
AleesMP <alejandromperis@gmail.com>
2024-04-18 19:58:47 +0200

    creacion colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'primary': '#2E3440',
+      'secondary': '#3B4252',
+      'tertiary': '#E5E9F0',
+    },
     extend: {},
   },
   plugins: [],
