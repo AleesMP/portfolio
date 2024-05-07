387dd97f4c9ff81015df9f84640c3e7e79e1f54f
AleesMP <alejandromperis@gmail.com>
2024-05-07 20:47:57 +0200

    envio formulario contacto

--- scripts.js
+++ scripts.js
@@ -45,39 +45,15 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", highlightMenuItem);
   });
   
+// Envio mail
+const $form = document.querySelector('#form')
+    const $buttonMailto = document.querySelector('#mailSender')
 
-// // Switcher oscuro/claro
-// var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-// var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+    $form.addEventListener('submit', handleSubmit)
 
-// if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//     themeToggleLightIcon.classList.remove('hidden');
-// } else {
-//     themeToggleDarkIcon.classList.remove('hidden');
-// }
-
-// var themeToggleBtn = document.getElementById('theme-toggle');
-
-// themeToggleBtn.addEventListener('click', function() {
-//     themeToggleDarkIcon.classList.toggle('hidden');
-//     themeToggleLightIcon.classList.toggle('hidden');
-
-//     if (localStorage.getItem('color-theme')) {
-//         if (localStorage.getItem('color-theme') === 'light') {
-//             document.documentElement.classList.add('dark');
-//             localStorage.setItem('color-theme', 'dark');
-//         } else {
-//             document.documentElement.classList.remove('dark');
-//             localStorage.setItem('color-theme', 'light');
-//         }
-//     } else {
-//         if (document.documentElement.classList.contains('dark')) {
-//             document.documentElement.classList.remove('dark');
-//             localStorage.setItem('color-theme', 'light');
-//         } else {
-//             document.documentElement.classList.add('dark');
-//             localStorage.setItem('color-theme', 'dark');
-//         }
-//     }
-// });
-
+    function handleSubmit(event) {
+      event.preventDefault()
+      const form = new FormData(this)
+      $buttonMailto.setAttribute('href', `mailto:alejandromperis@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+      $buttonMailto.click()
+    }
