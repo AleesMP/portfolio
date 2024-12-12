851b5e85d84ceb0b0f37ea646c13bac6d8f1844e
AleesMP <alejandromperis@gmail.com>
2024-12-12 14:58:27 +0100

    Añadido slider de tecnologias y ajustes del contenido

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
+    "./**/*.html", // Ajusta las rutas según la estructura de tu proyecto
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg_primary: "#2e3440",
+        text_primary: "#e5e9f0",
+        bg_secondary: "#3b4252",
+        bg_tertiary: "#434c5e",
+        bg_button: "#5e81ac",
+        color: "#5e81ac", // text-color
+      },
+      keyframes: {
+        "infinite-slide": {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        "infinite-slide": "infinite-slide 10s linear infinite",
+      },
+    },
+    fontFamily: {
+      firacode: ['"Fira Code"', "monospace"],
+    },
   },
   plugins: [],
-}
+};
